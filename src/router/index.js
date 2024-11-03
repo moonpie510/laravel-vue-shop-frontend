06aacc66d7788e3a786994870dd72522b20fc1aa
Denis <denstar510@yandex.ru>
2024-11-03 16:39:30 +0300

    16. Выводим варианты цветов в быстром просмотре. Добавление ресурсов и CRUD для Групп

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: () => import('../views/main/Index.vue')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/product/Index.vue')
+    },
+  ]
+})
+
+export default router
